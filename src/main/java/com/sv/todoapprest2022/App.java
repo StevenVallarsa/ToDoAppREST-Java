499e6dbb50b1bded1ae95795d8cf52b4824e704e
StevenVallarsa <orionsjv@gmail.com>
2022-01-23 19:12:45 -0500

    first commit

---
+++ src/main/java/com/sv/todoapprest2022/App.java
@@ -0,0 +1,20 @@
+
+package com.sv.todoapprest2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author: Steven Vallarsa
+ *   email: stevenvallarsa@gmail.com
+ *    date: 2022-01-23
+ * purpose: 
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
